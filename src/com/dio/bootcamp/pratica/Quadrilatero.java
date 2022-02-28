7018667daa7ae443289f8a4651dbf13f382aa247
Josafajosina <josafajosina@gmail.com>
2022-02-28 15:20:05 -0300

    Primeiro commit

---
+++ src/com/dio/bootcamp/pratica/Quadrilatero.java
@@ -0,0 +1,29 @@
+package com.dio.bootcamp.pratica;
+
+public class Quadrilatero {
+
+    public static void area(double lado) {
+
+        double result = lado * lado;
+
+        System.out.println("A área do Quadrado é igual a " + result);
+    }
+
+    public static void area(double lado1, double lado2) {
+        double result = lado1 * lado2;
+
+        System.out.println("A área do Retangulo é " + result);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        double baseMedia = (baseMaior + baseMenor) / 2;
+        //double baseMedia = somaDasBases / 2;
+        double result = baseMedia * altura;
+        System.out.println("A área do trapézio é " + result);
+    }
+}
+
+
+
+
---
+++ src/com/dio/bootcamp/pratica/main.java
@@ -0,0 +1,12 @@
+package com.dio.bootcamp.pratica;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("Resultado das àreas :");
+        Quadrilatero.area(4);
+        Quadrilatero.area(4,8);
+        Quadrilatero.area(10,8,12);
+
+    }
+}
